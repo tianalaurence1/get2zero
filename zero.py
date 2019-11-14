b06f014586d3d022e9863ef135fdc77f799b1837
tianalaurence <tianalaurence@gmail.com>
2019-11-14 14:10:10 -0600

    added the CO2 cost of one meal that is meat

--- zero.py
+++ zero.py
@@ -5,16 +5,19 @@
 
 
 '''Some variables that relate to carbon outputs and inputs'''
+
 ton = 20  # The metric tonnage of co2 produced by an average american
 pound_c = ton * 2205  # The number of pounds in a metric ton 2205, this is the pounds of carbon per american
 tree_in = 13  # The number of pounds co2 a young tree consumes a year
 tree_space = 12 * 12  # The space in feet a single tree needs to grow
 feet_space = int(pound_c / tree_in * tree_space)  # the space in feet all the trees need
 acre_space = int(feet_space / 43560)  # The square feet in an acre 43560, this is to find space in acres for trees
-hectare_space = int(feet_space / 107640)  # There are 107639.1 square feet in a hector, this is to hid hector space for trees
+hectare_space = int(feet_space / 107640)  # 107639.1 square feet in a hector, this is to hid hector space for trees
 meter_space = int(feet_space / 11)  # There are 10.76 feet in a meter
 trees = int(pound_c / tree_in + 1)
+meat_meal = int(7 - 3)  # CO2 in meat 6.61 lbs CO2 and beans to replace the calories at 2.25 lbs CO2
 
 print("The pounds of CO2 produced a year:", pound_c)
 print("The number of trees:", trees)
 print("The number of acres:", acre_space)
+print("CO2 output of one meal with meat:", meat_meal)
